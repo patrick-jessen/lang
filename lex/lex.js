56eede158536e60402c2d9c58beda878908f448f
Patrick Jessen <patrick.jessen@gmail.com>
2017-09-30 13:12:33 +0200

    Before array tokens

--- lex/lex.js
+++ lex/lex.js
@@ -1,18 +1,37 @@
-// Token fractions
-let TYPE = String.raw`string|int|float`
-let IDENTIFIER = String.raw`[a-zA-Z][a-zA-Z0-9_]*`
+var t = require('./tokens')
+console.log(t)
 
 // Tokens
+let KEY_WORD = String.raw`^var`
+let IDENTIFIER = String.raw`^[a-zA-Z][a-zA-Z0-9_]*`
+let LEFT_PAREN = String.raw`^\(`
+let RIGHT_PAREN = String.raw`^\)`
+let COMMA = String.raw`^,`
+
+// Literals
+let STRING = String.raw`^'[^']*'`
+let FLOAT = String.raw`^[0-9]+.[0-9]+`
+let INTEGER = String.raw`^[0-9]+`
+
+// Operators
+let EQUALS = String.raw`^=`
+
+// White space
+let LINE_BREAK = String.raw`^\n`
 let WHITE_SPACE = String.raw`^\s+`
-let VAR_DECL = String.raw`^var (${IDENTIFIER})( = (.*))?$`
-let FUNCTION_CALL = String.raw`^(${IDENTIFIER})\((.*)\)$`
+
+// Comments
+let COMMENT = String.raw`^\/\/.*\n`
+let BLOCK_COMMENT = String.raw`^\/\*[\s\S]*\*\/`
+
+let tokens = []
 
 let source = `
-print("msg")
+print(12.0)
 `
 function match(src, regs) {
     for(let i in regs) {
-        let m = src.match(new RegExp(regs[i], 'm'))
+        let m = src.match(new RegExp(regs[i]))
         if(m) {
             return {
                 regex: regs[i],
@@ -23,62 +42,107 @@ function match(src, regs) {
     return false
 }
 
-function parseBlock(src) {
+// Start the lexing
+function startLex(src) {
     let iter = 0
 
     while(iter < src.length) {
-        iter += parseStatement(src.substring(iter))
+        iter += lex(src.substring(iter))
     }
+    console.log(tokens)
 }
 
-function parseStatement(src) {
-    let regs = [WHITE_SPACE, VAR_DECL, FUNCTION_CALL]
+function lex(src) {
+    let regs = [
+        // Comments
+        COMMENT, BLOCK_COMMENT, 
+        // White space
+        LINE_BREAK, WHITE_SPACE, 
+        // Symbols
+        LEFT_PAREN, RIGHT_PAREN, COMMA,
+        // Literals
+        STRING, FLOAT, INTEGER,
+        // Keywords / names
+        KEY_WORD, IDENTIFIER, 
+        // Operators
+        EQUALS]
 
     let m = match(src, regs)
 
     switch(m.regex) {
+        case COMMENT:
+            break
+        case BLOCK_COMMENT:
+            break
+        case LINE_BREAK:
+            if(tokens.length > 0 && tokens[tokens.length-1].token != 'line_break') {
+                tokens.push({
+                    token: 'line_break'
+                })
+            }
+            break
         case WHITE_SPACE:
-            return parseWhiteSpace(src, m.match)
-        case VAR_DECL:
-            return parseVarDecl(src, m.match)
-        case FUNCTION_CALL:
-            return parseFunctionCall(src, m.match)
+            break
+        case LEFT_PAREN:
+            tokens.push({
+                token: 'left_paren'
+            })
+            break
+        case RIGHT_PAREN:
+            tokens.push({
+                token: 'right_paren'
+            })
+            break
+        case STRING:
+            tokens.push({
+                token: 'string',
+                value: m.match[0]
+            })
+            break
+        case FLOAT:
+            tokens.push({
+                token: 'float',
+                value: m.match[0]
+            })
+            break
+        case INTEGER:
+            tokens.push({
+                token: 'integer',
+                value: m.match[0]
+            })
+            break
+        case KEY_WORD:
+            tokens.push({
+                token: 'key_word',
+                value: m.match[0]
+            })
+            break
+        case IDENTIFIER:
+            tokens.push({
+                token: 'identifier',
+                value: m.match[0]
+            })
+            break
+        case EQUALS:
+            tokens.push({
+                token: 'equals'
+            })
+            break
+        case COMMA:
+            tokens.push({
+                token: 'comma'
+            })
+            break
         default:
             console.warn('unknown statement:', src)
             return 1
     }
+
+    return m.match[0].length
 }
-
-function parseWhiteSpace(src, match) {
-    // Do not do anything
-    return match.length
-}
-
-function parseVarDecl(src, match) {
-    let name = match[1]
-    let assign
-    if(match.length > 3)
-        assign = match[3]
-
-    console.log('VarDecl:', name, (assign ? assign : ''))
-
-    return match[0].length
-}
-
-function parseFunctionCall(src, match) {
-    let name = match[1]
-    let args = []
-    if(match[2].length > 0)
-        args = match[2].split(', ')
-
-    console.log('FunctionCall:', name, args)
-
-    return match[0].length
-}
-
 
 module.exports = {
     test() {
-        parseBlock(source)
+        startLex(source)
     }
 }
